578515b7ffb9972d5000f2af74064797387ca351
tomcsojn <44970244+tomcsojn@users.noreply.github.com>
2019-11-01 11:22:16 +0100

    get,add

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,26 +1,73 @@
-import axios,{
+import axios, {
     AxiosResponse,
     AxiosError
 } from "../../node_modules/axios/index"
 
 //url for the rest webservice at Azure
-let carWebUrl: string = "https://webapicar20190326034339.azurewebsites.net/api/cars/";
+let WebUrl: string = "https://itemservice-habo.azurewebsites.net/api/localitems";
+
+interface Item {
+    id: number,
+    name: string,
+    quality: string,
+    quantity: number
+}
 
 //create a click eventlistener at "Add" button
-let AddCarButton: HTMLButtonElement = <HTMLButtonElement> document.getElementById("addButton");
-AddCarButton.addEventListener('click',addCar);
+let AddCarButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("addButton");
+AddCarButton.addEventListener('click', add);
+let getallbutton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("getAllButton");
+getallbutton.addEventListener('click', getall);
+let gettable: HTMLTableElement = <HTMLTableElement>document.getElementById("gettable");
+
+function getall(): void {
+
+    console.log("At selectAllCarsfunction");
+
+    //axios call
+
+    axios.get<Item[]>(WebUrl)
+        .then(function (response: AxiosResponse<Item[]>) {
+            //then the get is ok
+            gettable.innerHTML = "";
+            response.data.forEach((car: Item) => {
+                gettable.innerHTML = gettable.innerHTML + "<tr> <td>" + car.id + "</td> <td>" + car.name + "</td><td>" + car.quality + "</td><td>" + car.quantity + "</td></tr>"
+
+            });
+
+        })
+        .catch(function (error: AxiosError) {
+            //then the get fails
+        });
+
+
+}
+
 
 //we need to code the axios post in this method
-function addCar():void{
+function add(): void {
 
     //Steps to do a axios post
-
     //step 1 we need to get the data from the html page (text input fields)
+    let idelement: HTMLInputElement = <HTMLInputElement>document.getElementById("idin");
+    let nameelement: HTMLInputElement = <HTMLInputElement>document.getElementById("namein");
+    let quantityelement: HTMLInputElement = <HTMLInputElement>document.getElementById("quantityin");
+    let qualityelement: HTMLInputElement = <HTMLInputElement>document.getElementById("qualityin");
 
-    //step 2 we need to create a json object with the data
+    let id: number = +idelement.value;
+    let name: string = nameelement.value;
+    let quality: string = qualityelement.value;
+    let quantity: number = +quantityelement.value;
 
-    //step 3 we need to do the axios post call with the data to the webservice
 
-    //step 4 we need to check if the data is stored  
-
+    axios.post<Item>(WebUrl, {
+        id: id,
+        name: name,
+        quality: quality,
+        quantity: quantity
+    })
+        .then(function (response: AxiosResponse) {
+            console.log("response " + response.status + " " + response.statusText)
+        })
+        .catch(function (error: AxiosError) { console.log(error) });
 }
